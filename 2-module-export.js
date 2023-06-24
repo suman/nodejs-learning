c1015cf049a4a9f997b9cf8d08e3ebf6d15e0a06
sumanbogati <suman.b@celestialsys.com>
2023-06-24 18:03:20 +0530

    Expport module in nodejs

---
+++ 2-module-export.js
@@ -0,0 +1,9 @@
+function sayHello (name) {
+    console.log(`Hello ${name} !!!`)
+}
+
+function sayCityName (name, city) {
+    console.log(`Awesome!!! ${name}, you are living in awesome city     ${city}!.`);
+}
+
+module.exports = {sayHello, sayCityName};
---
+++ app.js
@@ -0,0 +1,4 @@
+const greeting = require('./2-module-export.js');
+console.log("greeting ", greeting);
+greeting.sayHello('raj kumar');
+greeting.sayCityName("mukesh",  "mumbai");
