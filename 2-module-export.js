39700141281fa4ddcaac256979e23028cd314cae
suman <sumanbogati@gmail.com>
2023-08-17 20:35:37 +0530

    Add curd operation/apis with express

--- 2-module-export.js
+++
@@ -1,9 +0,0 @@
-function sayHello (name) {
-    console.log(`Hello ${name} !!!`)
-}
-
-function sayCityName (name, city) {
-    console.log(`Awesome!!! ${name}, you are living in awesome city     ${city}!.`);
-}
-
-module.exports = {sayHello, sayCityName};
--- app.js
+++ app.js
@@ -1,18 +1,74 @@
-const {readFile, writeFile} = require('fs');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
 
-const getFileText = (fileUrl) => {
-    return new Promise((resolve, reject) => {
-        readFile(fileUrl, 'utf8', (err, data) => {
-            if (err) {
-                reject(err);
-            }
-            resolve(data);
-        })
-    })
-}
+let {people} = require('./data');
 
-const result1 =  getFileText('./content/article/first-article.txt');
-result1.then((result) => console.log("First result: ", result));
+app.get('/people', (req, res) => {
+    res.json(people);
+})
 
-const result2 =  getFileText('./content/article/second-article.txt');
-result2.then((result) => console.log("Second result: ", result));
+let jsonParser = bodyParser.json();
+
+app.get('/people/:userId', (req, res) => {
+    console.log("something received from frontend")
+    const {userId} = req.params;
+    console.log('user id ', userId)
+    const foundUser = people.find((user) => user.id == userId)
+    console.log('===> ', foundUser)
+    if (foundUser) {
+        res.json(foundUser);
+    } else {
+        res.send({msg: 'User not found'});
+    }
+})
+
+app.put('/people/:userId', jsonParser, (req, res) => {
+    console.log("something received from frontend ", req.body)
+    const {userId} = req.params;
+    const name = req.body.name;
+    console.log('user id ', userId)
+    const foundUser = people.find((user) => user.id == userId)
+    console.log('===> ', foundUser)
+    if (foundUser) {
+        foundUser.name = name;
+        res.json(foundUser);
+    } else {
+        res.send({msg: 'User not found'});
+    }
+})
+
+app.post('/people/', jsonParser, (req, res) => {
+    console.log("something received from frontend ", req.body)
+    const user = req.body;
+    if (user) {
+        people.push(user);
+        res.json(user);
+    } else {
+        res.send({msg: 'User not found'});
+    }
+});
+
+
+app.delete('/people/:userId', (req, res) => {
+    console.log("something received from frontend")
+    const {userId} = req.params;
+    console.log('user id ', userId)
+    const foundUsers = people.filter((user) => user.id != userId)
+    console.log('===> ', foundUsers)
+    if (foundUsers) {
+        people = foundUsers;
+        res.json(foundUsers);
+    } else {
+        res.send({msg: 'User not found'});
+    }
+})
+
+
+app.get('*', (req, res) => {
+    res.status(400).send({msg: 'Path not found!!!'});
+})
+
+app.listen(5000, () => {
+    console.log("server is running at 5000")
+});
